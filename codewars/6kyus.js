c81e93b5fd009b58b25f26893b2d3b96678d0377
JongHyeokLee <jonghyeoklee90@gmail.com>
2020-02-01 00:01:26 +0100

    multiples of 3 or 5

--- codewars/6kyus.js
+++ codewars/6kyus.js
@@ -16,3 +16,17 @@ function findOdd(A) {
 }
 
 findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]);
+
+----------------Multiples of 3 or 5----------------
+
+function solution(n){
+  let arr = [];
+  for(let i = 0; i < n; i++) {
+        if(i % 3 === 0) {
+            arr.push(i);
+        } else if(i % 5 === 0) {
+            arr.push(i);
+        }
+  }
+  return arr.reduce((a,b) => a + b, 0);
+}
