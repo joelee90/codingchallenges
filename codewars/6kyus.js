def65534ef2d034ed2441886daad435af7bacdf4
JongHyeokLee <jonghyeoklee90@gmail.com>
2020-01-31 01:01:13 +0100

    find the odd int

---
+++ codewars/6kyus.js
@@ -0,0 +1,18 @@
+----------------Find the odd int----------------
+
+function findOdd(A) {
+  let count = 0;
+  let arr = A.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        count++;
+      }
+    }
+    if (count % 2 !== 0) {
+      return arr[i];
+    }
+  }
+}
+
+findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]);
